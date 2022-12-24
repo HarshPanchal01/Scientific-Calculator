22bba98c256621b41208b30062ca413619efabfb
Harsh <harshpanchal30x@gmail.com>
2022-12-24 14:35:10 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,60 @@
+const screen = document.querySelector('#screen');
+const buttons = document.querySelectorAll('[data-button]');
+
+buttons.forEach(button => {
+    button.addEventListener('click', (x) => {
+        buttonText = x.target.innerText;
+        if (buttonText === '×') {
+            buttonText = '*';
+        }
+        if (buttonText === '÷') {
+            buttonText = '/';
+        }
+        screen.value += buttonText;
+    });
+});
+
+function sin() {
+    screen.value = Math.sin(screen.value);
+}
+
+function cos() {
+    screen.value = Math.cos(screen.value);
+}
+
+function tan() {
+    screen.value = Math.tan(screen.value);
+}
+
+function pow() {
+    screen.value = Math.pow(screen.value, 2);
+}
+
+function sqrt() {
+    screen.value = Math.sqrt(screen.value, 2);
+}
+
+function log() {
+    screen.value = Math.log(screen.value);
+}
+
+function pi() {
+    screen.value = Math.PI;
+}
+
+function fact() {
+    var i, num, f;
+    f = 1
+    num = screen.value;
+    for (i = 1; i <= num; i++) {
+        f = f * i;
+    }
+
+    i = i - 1;
+
+    screen.value = f;
+}
+
+function backspc() {
+    screen.value = screen.value.substr(0, screen.value.length - 1);
+}
